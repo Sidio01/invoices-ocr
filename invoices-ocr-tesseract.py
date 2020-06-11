8924b31ef396ccdf92031a58571a1a6062f035e3
Sidio01 <sidio1992@gmail.com>
2020-06-11 18:02:33 +0300

    Import cv2

--- invoices-ocr-tesseract.py
+++ invoices-ocr-tesseract.py
@@ -1,49 +1,25 @@
-try:
-    from PIL import Image
-except ImportError:
-    import Image
 import pytesseract
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 # If you don't have tesseract executable in your PATH, include the following:
-# pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 # Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
-# Simple image to string
-print(pytesseract.image_to_string(Image.open('schet.png')))
+img = cv2.imread('schet.png')
 
-# French text image to string
-# print(pytesseract.image_to_string(Image.open('test-european.jpg'), lang='fra'))
+# Adding custom options
+custom_config = r'--oem 3 --psm 6'
+print(pytesseract.image_to_string(img, config=custom_config, lang='rus'))
 
-# In order to bypass the image conversions of pytesseract, just use relative or absolute image path
-# NOTE: In this case you should provide tesseract supported images or tesseract will return error
-# pytesseract.image_to_string('schet.png')
 
-"""
-# Batch processing with a single file containing the list of multiple image file paths
-print(pytesseract.image_to_string('images.txt'))
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Timeout/terminate the tesseract job after a period of time
-try:
-    print(pytesseract.image_to_string('test.jpg', timeout=2)) # Timeout after 2 seconds
-    print(pytesseract.image_to_string('test.jpg', timeout=0.5)) # Timeout after half a second
-except RuntimeError as timeout_error:
-    # Tesseract processing is terminated
-    pass
 
-# Get bounding box estimates
-print(pytesseract.image_to_boxes(Image.open('test.png')))
-
-# Get verbose data including boxes, confidences, line and page numbers
-print(pytesseract.image_to_data(Image.open('test.png')))
-
-# Get information about orientation and script detection
-print(pytesseract.image_to_osd(Image.open('test.png')))
-
-# Get a searchable PDF
-pdf = pytesseract.image_to_pdf_or_hocr('test.png', extension='pdf')
-with open('test.pdf', 'w+b') as f:
-    f.write(pdf) # pdf type is bytes by default
-
-# Get HOCR output
-hocr = pytesseract.image_to_pdf_or_hocr('test.png', extension='hocr')
-"""
+a = get_grayscale(img)
+plt.imshow(a, cmap='Greys')
+plt.show()
+plt.imshow(img, cmap='Greys')
+plt.show()
